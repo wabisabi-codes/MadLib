a6852611bdd289db7e872b72be14af360f667746
Ray Wace <swi3179@wgu.edu>
2023-05-02 11:45:59 -0500

    base html, styling css, and javascript practice

---
+++ javascript.js
@@ -0,0 +1,81 @@
+function greatEscape() {
+  const Username = prompt("Enter a name: ");
+  const adverb1 = prompt("Enter an adverb: ");
+  const adjective1 = prompt("Enter an adjective: ");
+  const noun1 = prompt("Enter a noun: ");
+  const verb = prompt("Enter a verb: ");
+  const occupation = prompt("Enter an occupation: ");
+  const noun2 = prompt("Enter a noun: ");
+
+  document.write(
+    Username +
+      " quickly " +
+      adverb1 +
+      " realized they were trapped in a " +
+      adjective1 +
+      noun1 +
+      "and needed to " +
+      verb +
+      " their way out. Using their skills as a " +
+      occupation +
+      ", they made a makeshift " +
+      noun2 +
+      " and escaped to safety."
+  );
+}
+
+function mDisappearance() {
+  const Username2 = prompt("Enter a name: ");
+  const adjective11 = prompt("Enter an adjective: ");
+  const adjective12 = prompt("Enter an adjective: ");
+  const occupation1 = prompt("Enter an occupation: ");
+  const adjective13 = prompt("Enter an adjective: ");
+  const noun11 = prompt("Enter a noun: ");
+
+  document.write(
+    "When " +
+      Username2 +
+      " vanished without a trace, the townsfolk were " +
+      adjective11 +
+      " and " +
+      adjective12 +
+      " . A " +
+      occupation1 +
+      " was called in to investigate, but no clues could be found. It wasn't until a " +
+      adjective13 +
+      noun11 +
+      " was discovered that the truth was revealed."
+  );
+}
+
+function interBattle() {
+  const occupation2 = prompt("Enter an occupation: ");
+  const Username3 = prompt("Enter a name: ");
+  const adjective11 = prompt("Enter an adjective: ");
+  const noun12 = prompt("Enter a noun: ");
+  const adjective14 = prompt("Enter an adjective: ");
+  const noun13 = prompt("Enter a noun: ");
+  const adjective15 = prompt("Enter an adjective: ");
+  const noun14 = prompt("Enter a noun: ");
+  const noun15 = prompt("Enter a noun: ");
+
+  document.write(
+    "As a " +
+      occupation2 +
+      ", " +
+      Username3 +
+      " was used to " +
+      adjective11 +
+      noun12 +
+      ", but nothing could have prepared them for the " +
+      adjective14 +
+      "battle they were about to face. Armed with only their " +
+      noun13 +
+      ", they stood against the " +
+      adjective15 +
+      noun14 +
+      "and fought for the fate of the " +
+      noun15 +
+      "."
+  );
+}
